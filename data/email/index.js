3bb3dbf8695ed1aab906e56779e041c8bac028a2
David Eastmond <davebuck1981@gmail.com>
2023-04-02 08:33:58 -0400

    removed e-mail data stuff as it now lives in the webhook server

--- data/email/index.js
+++
@@ -1,8 +0,0 @@
-import { PasswordRecoveryEmail } from "./password-recovery-email/password-recovery-email.js";
-import { PasswordChangedEmail } from "./password-changed-email/password-changed-email.js";
-import { SignUpEmail } from "./sign-up-email/sign-up-email.js";
-export { 
-	PasswordRecoveryEmail,
-	PasswordChangedEmail,
-	SignUpEmail
-}
