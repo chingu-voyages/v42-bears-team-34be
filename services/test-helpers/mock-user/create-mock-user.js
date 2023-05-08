c4f4a357b1456a56083928caa4cb15f81cb3e573
David Eastmond <davebuck1981@gmailcom>
2023-05-08 10:28:11 +0200

    linting fix

--- services/test-helpers/mock-user/create-mock-user.js
+++ services/test-helpers/mock-user/create-mock-user.js
@@ -5,25 +5,25 @@ import dayjs from 'dayjs';
 import User from '../../../schemas/user.js';
 
 function generateUserData(
-	idx = 1,
-	firstName = 'muFirstName',
-	lastName = 'muLastName',
-	dateOfBirth = dayjs().subtract(19, 'years').toDate(),
-	email = 'mockEmail',
-	applicantGender = 'male',
-	password = 'Password$123',
-	role = 'user'
+  idx = 1,
+  firstName = 'muFirstName',
+  lastName = 'muLastName',
+  dateOfBirth = dayjs().subtract(19, 'years').toDate(),
+  email = 'mockEmail',
+  applicantGender = 'male',
+  password = 'Password$123',
+  role = 'user'
 ) {
-	return {
-		firstName: firstName + idx,
-		lastName: lastName + idx,
-		dateOfBirth,
-		email: `${email}${idx}@example.com`,
-		applicantGender,
-		dateSignedUp: dayjs().toDate(),
-		hashedPassword: bcrypt.hashSync(password, 10),
-		role,
-	};
+  return {
+    firstName: firstName + idx,
+    lastName: lastName + idx,
+    dateOfBirth,
+    email: `${email}${idx}@example.com`,
+    applicantGender,
+    dateSignedUp: dayjs().toDate(),
+    hashedPassword: bcrypt.hashSync(password, 10),
+    role,
+  };
 }
 /**
  *
@@ -36,29 +36,29 @@ function generateUserData(
  * @returns {Promise<any[]>} Array of create mongo user documents
  */
 async function createMockUser(
-	count,
-	firstName,
-	lastName,
-	email,
-	password,
-	role
+  count,
+  firstName,
+  lastName,
+  email,
+  password,
+  role
 ) {
-	const dummies = [];
-	for (let i = 1; i <= count; i += 1) {
-		dummies.push(
-			generateUserData(
-				i,
-				firstName,
-				lastName,
-				undefined,
-				email,
-				undefined,
-				password,
-				role
-			)
-		);
-	}
-	return Promise.all(dummies.map((dummy) => User.create(dummy)));
+  const dummies = [];
+  for (let i = 1; i <= count; i += 1) {
+    dummies.push(
+      generateUserData(
+        i,
+        firstName,
+        lastName,
+        undefined,
+        email,
+        undefined,
+        password,
+        role
+      )
+    );
+  }
+  return Promise.all(dummies.map((dummy) => User.create(dummy)));
 }
 
 export default createMockUser;
