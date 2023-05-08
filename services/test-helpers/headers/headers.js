75ac828c5145d9b6cc36786310661aa3dae309eb
David Eastmond <davebuck1981@gmailcom>
2023-05-08 10:24:52 +0200

    fix formatting

--- services/test-helpers/headers/headers.js
+++ services/test-helpers/headers/headers.js
@@ -1,5 +1,6 @@
+// eslint-disable-next-line import/prefer-default-export
 export const HEADERS = {
-    formUrlEncoded: {
-        'content-type': 'application/x-www-form-urlencoded'
-    }
+  formUrlEncoded: {
+    'content-type': 'application/x-www-form-urlencoded'
+  }
 }
