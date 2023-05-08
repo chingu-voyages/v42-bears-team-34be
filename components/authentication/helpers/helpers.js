a8cf2ec1ed6786c0439a817157ebe4ac12e20d79
David Eastmond <davebuck1981@gmailcom>
2023-05-08 10:30:19 +0200

    linting fix

--- components/authentication/helpers/helpers.js
+++ components/authentication/helpers/helpers.js
@@ -2,11 +2,11 @@ import User from '../../../schemas/user.js';
 import { IS_PRODUCTION } from '../../../services/environment.js';
 
 export function generatePasswordRecoveryURL(token) {
-	// We need to determine the development environment.
-	if (IS_PRODUCTION) {
-		return `${process.env.PRODUCTION_APP_DOMAIN}/password-reset/recover?token=${token}`;
-	}
-	return `${process.env.DEV_APP_DOMAIN}/password-reset/recover?token=${token}`;
+  // We need to determine the development environment.
+  if (IS_PRODUCTION) {
+    return `${process.env.PRODUCTION_APP_DOMAIN}/password-reset/recover?token=${token}`;
+  }
+  return `${process.env.DEV_APP_DOMAIN}/password-reset/recover?token=${token}`;
 }
 
 /**
@@ -15,7 +15,7 @@ export function generatePasswordRecoveryURL(token) {
  * @returns {Promise<boolean>}
  */
 export async function checkIfUserExistsInDb(email) {
-	const user = await User.findOne({ email });
+  const user = await User.findOne({ email });
   if (user) return true;
   return false;
 }
--- components/authentication/helpers/helpers.test.js
+++ components/authentication/helpers/helpers.test.js
@@ -1,22 +1,24 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { jest } from "@jest/globals";
-import User from "../../../schemas/user";
-import { checkIfUserExistsInDb } from "./helpers";
+import { jest } from '@jest/globals';
+import User from '../../../schemas/user';
+import { checkIfUserExistsInDb } from './helpers';
 
-jest.mock("../../../schemas/user");
+jest.mock('../../../schemas/user');
 
-beforeEach(() => jest.restoreAllMocks())
-describe("Helpers", () => {
-  describe("Check if user exists", () => {
-    test("Returns true if exists", async () => {
-      User.findOne.mockImplementation(() => Promise.resolve({ value: true }))
-      const res = await checkIfUserExistsInDb("dummy@example.com");
-      expect(res).toBe(true)
+beforeEach(() => jest.restoreAllMocks());
+describe('Helpers', () => {
+  describe('Check if user exists', () => {
+    test('Returns true if exists', async () => {
+      User.findOne.mockImplementation(() => Promise.resolve({ value: true }));
+      const res = await checkIfUserExistsInDb('dummy@example.com');
+      expect(res).toBe(true);
     });
-    test("Returns false", async () => {
-      jest.spyOn(User, "findOne").mockImplementation(() => Promise.resolve(null))
-      const res = await checkIfUserExistsInDb("dummy@example.com");
-      expect(res).toBe(false)
-    })
-  })
-})
+    test('Returns false', async () => {
+      jest
+        .spyOn(User, 'findOne')
+        .mockImplementation(() => Promise.resolve(null));
+      const res = await checkIfUserExistsInDb('dummy@example.com');
+      expect(res).toBe(false);
+    });
+  });
+});
