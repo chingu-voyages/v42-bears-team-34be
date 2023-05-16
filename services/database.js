934cc1be3f2bfed21acbe447a9ca9e0b3ceb77c2
David Eastmond <davebuck1981@gmailcom>
2023-05-16 11:28:41 +0200

    database.js lint fix

--- services/database.js
+++ services/database.js
@@ -1,23 +1,21 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-const db = {
-    initialize(){
-        const
-            user = process.env.MONGO_USER,
-            pass = process.env.MONGO_PASS,
-            base = process.env.MONGO_BASE,
-            db   = process.env.MONGO_DBNAME
-        const
-            mongoUri = `mongodb+srv://${user}:${pass}@${base}/${db}?retryWrites=true&w=majority`
+const database = {
+  initialize() {
+    const user = process.env.MONGO_USER;
+    const pass = process.env.MONGO_PASS;
+    const base = process.env.MONGO_BASE;
+    const db = process.env.MONGO_DBNAME;
+    const mongoUri = `mongodb+srv://${user}:${pass}@${base}/${db}?retryWrites=true&w=majority`;
 
-        mongoose.set('strictQuery', false);
+    mongoose.set('strictQuery', false);
 
-        console.log("DB is initialized here...")
+    console.info('DB is initialized here...');
 
-        mongoose.connect(mongoUri).then( ()=>{
-            console.log("Connected to mongo database!")
-        })
-    }
-}
+    mongoose.connect(mongoUri).then(() => {
+      console.info('Connected to mongo database!');
+    });
+  },
+};
 
-export default db
+export default database;
