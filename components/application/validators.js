d55faab159cedbd8cfdd529f51f011c7d378b65e
David Eastmond <davebuck1981@gmail.com>
2023-07-02 17:23:00 -0400

    linting for validators.js application

--- components/application/validators.js
+++ components/application/validators.js
@@ -6,53 +6,68 @@ import { protectedRoute } from '../../middleware/protectedRoute.js';
 import validationGuard from '../../middleware/validationGuard.js';
 
 export const applicationValidator = [
-    protectedRoute,
-    body('requestedLoanAmount').isNumeric(),
-    body('numberOfInstallments').isNumeric(),
-    body('installmentAmount').isNumeric(),
-    body('loanPurpose').exists().trim().escape(),
-    body('applicantOccupation').exists().trim().escape(),
-    body('applicantIncome').isNumeric(),
-    validationGuard
-]
+  protectedRoute,
+  body('requestedLoanAmount').isNumeric(),
+  body('numberOfInstallments').isNumeric(),
+  body('installmentAmount').isNumeric(),
+  body('loanPurpose').exists().trim().escape(),
+  body('applicantOccupation').exists().trim().escape(),
+  body('applicantIncome').isNumeric(),
+  validationGuard,
+];
 
 export const userApplicationQueryValidator = [
-    protectedRoute,
-    param('id').isHexadecimal().trim().isLength({min: 24, max: 24}).escape(),
-    validationGuard
-]
+  protectedRoute,
+  param('id').isHexadecimal().trim().isLength({ min: 24, max: 24 }).escape(),
+  validationGuard,
+];
 
 export const adminApplicationQueryValidator = [
-    adminRoute,
-    param('id').isHexadecimal().trim().isLength({min: 24, max: 24}).escape(),
-    validationGuard
-]
+  adminRoute,
+  param('id').isHexadecimal().trim().isLength({ min: 24, max: 24 }).escape(),
+  validationGuard,
+];
 
 export const adminApplicationRejectValidator = [
-    body("reason").exists().trim().escape(),
-    validationGuard,
-]
+  body('reason').exists().trim().escape(),
+  validationGuard,
+];
 
 export const adminApplicationPatchStatusValidator = [
-    body("action").custom((value) => ["mark_incomplete",
-        "mark_more_info_required", "update_reject_reason", "admin_cancel"].includes(value)),
-    body("message").optional().trim().escape()
-]
+  body('action').custom((value) =>
+    [
+      'mark_incomplete',
+      'mark_more_info_required',
+      'update_reject_reason',
+      'admin_cancel',
+    ].includes(value)
+  ),
+  body('message').optional().trim().escape(),
+];
 
 export const paymentSizeValidator = [
-    query("requestedLoanAmount").exists().isInt().custom(val => val > 0),
-    validationGuard
-]
+  query('requestedLoanAmount')
+    .exists()
+    .isInt()
+    .custom((val) => val > 0),
+  validationGuard,
+];
 
 export const triggerWelcomeEmailValidator = [
-    body("itemId").exists().isString(),
-    body("email").exists().isEmail(),
-    body("applicationId").exists().isString(),
-    validationGuard
-]
+  body('itemId').exists().isString(),
+  body('email').exists().isEmail(),
+  body('applicationId').exists().isString(),
+  validationGuard,
+];
 
 export const pageSearchQueryValidator = [
-    query("page").exists().isInt().custom((val) => val >= 0),
-    query("count").exists().isInt().custom((val) => val > 0),
-    validationGuard
-]
+  query('page')
+    .exists()
+    .isInt()
+    .custom((val) => val >= 0),
+  query('count')
+    .exists()
+    .isInt()
+    .custom((val) => val > 0),
+  validationGuard,
+];
